70743dfa8dd956105eebf9632ed96618b5ffe8ad
John <hotwire2990@gmail.com>
2019-01-15 18:57:39 -0800

    First working drivetrain with vision tracking

--- 2019_DeepSpace_Official/src/main/java/frc/robot/Robot.java
+++ 2019_DeepSpace_Official/src/main/java/frc/robot/Robot.java
@@ -1,98 +1,165 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot;
 
-import edu.wpi.first.wpilibj.IterativeRobot;
-import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Joystick;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.Talon;
+import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.cameraserver.CameraServer;
+import com.ctre.phoenix.motorcontrol.ControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonSRX;
+import edu.wpi.first.wpilibj.Ultrasonic;
+import edu.wpi.first.wpilibj.GenericHID.RumbleType;
+import edu.wpi.first.wpilibj.interfaces.Potentiometer;
+import edu.wpi.first.wpilibj.AnalogPotentiometer;
+import com.kauailabs.navx.frc.AHRS;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.networktables.NetworkTable;
+import edu.wpi.first.networktables.NetworkTableEntry;
+import edu.wpi.first.networktables.NetworkTableInstance;
 
-/**
- * The VM is configured to automatically run this class, and to call the
- * functions corresponding to each mode, as described in the IterativeRobot
- * documentation. If you change the name of this class or the package after
- * creating this project, you must also update the build.gradle file in the
- * project.
- */
-public class Robot extends IterativeRobot {
-  private static final String kDefaultAuto = "Default";
-  private static final String kCustomAuto = "My Auto";
-  private String m_autoSelected;
-  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+public class Robot extends TimedRobot {
 
-  /**
-   * This function is run when the robot is first started up and should be
-   * used for any initialization code.
-   */
-  @Override
-  public void robotInit() {
-    m_chooser.setDefaultOption("Default Auto", kDefaultAuto);
-    m_chooser.addOption("My Auto", kCustomAuto);
-    SmartDashboard.putData("Auto choices", m_chooser);
-  }
+	// Sensors
+	public AHRS navx = new AHRS(SPI.Port.kMXP);
+	public Ultrasonic Ultrasonic = new Ultrasonic(0, 1);
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use
-   * this for items like diagnostics that you want ran during disabled,
-   * autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before
-   * LiveWindow and SmartDashboard integrated updating.
-   */
-  @Override
-  public void robotPeriodic() {
-  }
+	// Drivetrain
+	public DriveTrain drivetrain;
+	public JoshMotorControllor badMotor = new JoshMotorControllor(5, 0.8f, false);
 
-  /**
-   * This autonomous (along with the chooser code above) shows how to select
-   * between different autonomous modes using the dashboard. The sendable
-   * chooser code works with the Java SmartDashboard. If you prefer the
-   * LabVIEW Dashboard, remove all of the chooser code and uncomment the
-   * getString line to get the auto name from the text box below the Gyro
-   *
-   * <p>You can add additional auto modes by adding additional comparisons to
-   * the switch structure below with additional strings. If using the
-   * SendableChooser make sure to add them to the chooser code above as well.
-   */
-  @Override
-  public void autonomousInit() {
-    m_autoSelected = m_chooser.getSelected();
-    // autoSelected = SmartDashboard.getString("Auto Selector",
-    // defaultAuto);
-    System.out.println("Auto selected: " + m_autoSelected);
-  }
+	// neumatics
 
-  /**
-   * This function is called periodically during autonomous.
-   */
-  @Override
-  public void autonomousPeriodic() {
-    switch (m_autoSelected) {
-      case kCustomAuto:
-        // Put custom auto code here
-        break;
-      case kDefaultAuto:
-      default:
-        // Put default auto code here
-        break;
-    }
-  }
+	// Joysticks
+	public Joystick driver;
+	public Joystick operator;
+	public Joystick debug;
 
-  /**
-   * This function is called periodically during operator control.
-   */
-  @Override
-  public void teleopPeriodic() {
-  }
+	// Auto
 
-  /**
-   * This function is called periodically during test mode.
-   */
-  @Override
-  public void testPeriodic() {
-  }
+	public void robotInit() {
+		drivetrain = new DriveTrain(9, 10, 7, 5, navx);
+	}
+
+	public void disabledPeriodic() {
+		// Controllers
+		driver = new Joystick(0);
+		operator = new Joystick(1);
+	}
+
+	public void autonomousInit() {
+
+		drivetrain.SetBreak();
+	}
+
+	public void autonomousPeriodic() {
+
+		UpdateMotors();
+	}
+
+	public void teleopInit() {
+		// Controllers
+		debug = new Joystick(3);
+		driver = new Joystick(0);
+		operator = new Joystick(1);
+
+		// Drivetrain
+		drivetrain.SetCoast();
+		float lerpSpeed = 0.5f;
+		drivetrain.SetLeftSpeed(lerpSpeed);
+		drivetrain.SetRightSpeed(lerpSpeed);
+	}
+
+	public void teleopPeriodic() {
+
+		driver.setRumble(RumbleType.kLeftRumble, 0);
+		drivetrain.SetLeftSpeed(0);
+		drivetrain.SetRightSpeed(0);
+
+		NetworkTable table = NetworkTableInstance.getDefault().getTable("limelight");
+		NetworkTableEntry tx = table.getEntry("tx");
+		NetworkTableEntry ty = table.getEntry("ty");
+		NetworkTableEntry ta = table.getEntry("ta");
+
+		double x = tx.getDouble(0.0);
+		double y = ty.getDouble(0.0);
+		double area = ta.getDouble(0.0);
+
+		// Driver Controls
+		ControllerDrive();
+
+		if (driver.getRawButton(5)) {
+
+			float approachSpeed = 0.2f;
+			float turnSpeed = 0.2f;
+			float buffer = 2.0f;
+			float approachtarget = 4;
+
+			if (x >= buffer) {
+				drivetrain.SetLeftSpeed(turnSpeed);
+				drivetrain.SetRightSpeed(-turnSpeed);
+			} else if (x <= -buffer) {
+				drivetrain.SetLeftSpeed(-turnSpeed);
+				drivetrain.SetRightSpeed(turnSpeed);
+			} else {
+				drivetrain.SetLeftSpeed(0f);
+				drivetrain.SetRightSpeed(0f);
+				driver.setRumble(RumbleType.kLeftRumble, 1);
+				if (y < approachtarget) {
+					drivetrain.SetLeftSpeed(approachSpeed);
+					drivetrain.SetRightSpeed(approachSpeed);
+				}
+			}
+		}
+
+		UpdateMotors();
+	}
+
+	public void testInit() {
+		// Controllers
+		debug = new Joystick(3);
+		driver = new Joystick(0);
+		operator = new Joystick(1);
+
+		// Drivetrain
+		float lerpSpeed = 0.5f;
+		drivetrain.SetLeftSpeed(lerpSpeed);
+		drivetrain.SetRightSpeed(lerpSpeed);
+	}
+
+	public void testPeriodic() {
+
+	}
+
+	public void UpdateMotors() {
+		drivetrain.Update();
+
+	}
+
+	public float TranslateController(float input) {
+		float deadzone = 0.15f;
+		if (input > -deadzone && input < deadzone) {
+			input = 0.0f;
+		}
+		float a = 0.7f;
+		float output = (a * input * input * input) + (1 - a) * input;
+		return output;
+	}
+
+	// custom classes
+	public void LogInfo(String info) {
+		System.out.println(info + ";    ");
+	}
+
+	public void ControllerDrive() {
+		float horJoystick = TranslateController((float) driver.getRawAxis(0));
+		float verJoystick = TranslateController((float) driver.getRawAxis(5));
+
+		drivetrain.SetRightSpeed(-verJoystick + -horJoystick);
+		drivetrain.SetLeftSpeed(-verJoystick + horJoystick);
+	}
 }
